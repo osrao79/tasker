2dae30010c9a75327a95adc4fb4a016b07e1aab8
osrao79 <onkar.rao@classicinformatics.com>
2023-06-30 12:53:28 +0530

    file restructure

--- build/index.js
+++ build/index.js
@@ -4,9 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const config_1 = require("../config");
-const db_1 = __importDefault(require("../db"));
-const routes_1 = __importDefault(require("../routes/routes"));
+const config_1 = require("./config");
+const db_1 = __importDefault(require("./db"));
+const routes_1 = __importDefault(require("./routes/routes"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors = require('cors');
 const app = (0, express_1.default)();
